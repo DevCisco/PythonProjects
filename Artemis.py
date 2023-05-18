edb0d47c6272ed1bfa1e719165d8c3334f737f8c
Francesco Pisaturo <97796423+DevCisco@users.noreply.github.com>
2023-05-18 12:08:49 +0200

    Delete Artemis.py

--- Artemis.py
+++
@@ -1,9 +0,0 @@
-#lettura e scrittura di file testuale
-fileobject=open("text.txt","w")
-fileobject.write("First Astronaut to Moon\n")
-fileobject.write("Neil A.\n")
-fileobject.close()
-fileobject=open("text.txt")
-textLine=fileobject.readlines()
-for line in textLine:
-    print(line)
