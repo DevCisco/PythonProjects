053895d1b6d9d45eb588564087673e1741f666d6
Francesco Pisaturo <97796423+DevCisco@users.noreply.github.com>
2024-12-21 10:41:11 +0100

    Update luhnalgorithm.py
    This algorithm can be used in order to verify the check digit of a number. The process calculates the sum of all the numbers and then procedes to divide it by 10. If the module is 0, the number is correct, otherwise it must be reinserted.

--- luhnalgorithm.py
+++ luhnalgorithm.py
@@ -4,15 +4,15 @@ def verificaluhn():
     n=[]
     for i in range(0,x):
         element=int(input("Inserire la cifra della carta: "))
-        n.append(element)
+        n.insert(i,element)
     print("ciccio", n)
     for i in range (x,0):
         if i%2==0:
             n[i]=n[i]*2
             if n[i]>10:
                 n[i]=(n[i]%2)+1
-    for i in range (x,0):
-       somma=sum(n[i])
+    for num in n:
+       somma=somma+num
     if ((somma%10)==0):
         print("Corretto")
     else:
