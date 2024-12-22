00bc96e5db4aa518d0fcbf9a14e93762ad702bca
Francesco Pisaturo <97796423+DevCisco@users.noreply.github.com>
2024-12-22 19:44:16 +0100

    Updating the algorithm

--- luhnalgorithm.py
+++ luhnalgorithm.py
@@ -1,29 +1,24 @@
 import time
 def verificaluhn():
     somma=0
-    x=15
-    n=[]
-    for i in range(0,x):
+    x=16
+    for i in range(x-1):
         element=int(input("Inserire la cifra della carta: "))
-        if i==x-1:
-            somma+=element
-            continue
         if (i%2)==0:
            element=element*2
            if element>=10:
-                element=(element%2)+1
-        somma=somma+element
+                element=element-9
+        somma+=element
     z=int(input("Inserire l'ultima cifra della carta: "))
     y=somma%10
-    if ((10-y)==z):
+    if (((10-y)%10)==z):
         print("Corretto")
     else:
         print("Non corretto, inserire nuovamente il numero")
         verificaluhn()
-toto=time.localtime()
-start_time = time.time()
-end_time=time.localtime()
-verificaluhn()
-print("Start time: ", toto)
-print("Finish Time: ", end_time)
-print("Execution time: %s seconds" % (time.time() - start_time))
+
+def main():
+    verificaluhn()
+
+if __name__ == "__main__":
+    main()
